c0b67ea867175a2c150f1ef484ac64f279c0c18c
aiman <nooraimanmohd@gmail.com>
2024-05-20 08:49:12 +0800

    chore: add db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,26 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Student;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $faker = Faker::create();
+        $gender = $faker->randomElement(['male', 'female']);
+        foreach (range(1,200) as $index) {
+            Student::insert([
+                'name'  => $faker->name($gender),
+                'email' => $faker->email,
+                'phone' => $faker->phoneNumber,
+            ]);
+        }
     }
 }
